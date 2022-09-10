c94daa40873e3f0b0cf575ef843efcd43e2dbb0f
jackson-weber <jacksonweber20@gmail.com>
2022-09-10 11:26:51 -0400

    wrote path util functions

--- path.c
+++ path.c
@@ -3,6 +3,55 @@
 #include "path.h"
 #include "mazehelper.h"
 
+bool check_path(Maze * m, char * path)
+{
+	MazePos cur = { .xpos = m -> start.xpos, .ypos = m -> start.ypos };
+	
+	while (path [pathPos])
+	{
+		printf("At position %d. Maze positionL (%d, %d), Square type: %c, Next step: %c\n", 
+			pathPos, cur.xpos, cur.ypos, m -> maze[cur.ypos][cur.xpos].type, path[pathPos]);
+
+		//Take a step
+		switch(path[pathPos])
+		{
+			case NORTH: cur.ypos--; break;
+			case SOUTH: cur.ypos++; break;
+			case EAST:  cur.xpos++; break;
+			case WEST: 	cur.xpos--; break;
+			default: fprintf(stderr, "Incorrectly formatted path string\n"); exit(1);
+		}
+		
+		//Make sure square is in bounds before we read it 
+		if (!in_bounds(cur, m))
+		{
+			return false;
+		}
+
+		printf("Moved to square: (%d, %d), Square type: %c\n",
+			cur.xpos, cur.ypos, m -> maze[cur.ypos][cur.xpos].type);
+			
+		//Check if square is valid
+		if (!square_valid(cur, m))
+		{
+			return false;	
+		}
+	
+		m -> maze[cur.ypos][cur.xpos].visited = true;
+
+		if (at_end(cur, m))
+		{
+			return true;
+		}
+		
+		pathPos++;
+	
+		//Will return false until we are at the end of the maze
+		//Used as helper for recursive DFS implementation in solver.c
+		return false;
+	}
+}
+
 char * read_path(char * pathFileName)
 {
 	FILE * inputPath = fopen(pathFileName, 'r');
@@ -12,6 +61,7 @@ char * read_path(char * pathFileName)
 		fprintf(stderr, "Error opening input path file\n");
 	}
 	
+	//Get length of file
 	int len = 0;
 	while (!feof(inputPath))
 	{
@@ -19,6 +69,7 @@ char * read_path(char * pathFileName)
 		len++;
 	}
 
+	//Go back to start
 	if (fseek(inputPath, 0L, SEEK_SET) == -1)
 	{
 		fprintf(stderr, "Problem seeking to beginning of file\n");
@@ -27,6 +78,7 @@ char * read_path(char * pathFileName)
 
 	char * path = malloc(len * sizeof(char));
 	
+	//Read each line into path
 	fscanf(inputPath, "%s\n", path);
 
 	if (fclose(inputPath))
@@ -48,6 +100,7 @@ int write_path(char * pathFileName, char * path)
 		return EXIT_FAILURE;
 	}
 	
+	//Print out final path
 	fprintf(f, "%s", path);
 	
 	fclose(f);
@@ -55,4 +108,3 @@ int write_path(char * pathFileName, char * path)
 	return EXIT_SUCCESS;
 	
 }
-
